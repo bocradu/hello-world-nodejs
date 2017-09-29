d24c88242969f546b26ed599aebcf9c440119c1a
DM User <dockerservice@vltmiwapp01.haufe-ep.de>
2017-09-29 13:51:49 +0000

    first commit

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+
+app.use(function (req, res, next) {
+   res.send("Hello World 1");
+});
+
+app.set('port', 80);
+
+var server = app.listen(app.get('port'), function() {
+    console.log('Express server listening on port ' + server.address().port);
+});
