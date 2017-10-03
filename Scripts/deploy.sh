c95e409440385e4f35736ad6b66154bcc6c91d91
Boc <radu.boc@haufe-lexware.com>
2017-10-03 16:09:32 +0300

    Fixed script

--- Scripts/deploy.sh
+++ Scripts/deploy.sh
@@ -6,4 +6,10 @@ if [ $liveVersion == "blue" ]; then
 else
 	docker-compose -f ../$2/docker-compose.yml up -d blue
 fi
+if [ $(docker inspect -f {{.State.Running}} blue) == false ]; then
+	docker-compose -f ../$2/docker-compose.yml up -d blue
+fi
+if [ $(docker inspect -f {{.State.Running}} green) == false ]; then
+	docker-compose -f ../$2/docker-compose.yml up -d green
+fi
 
