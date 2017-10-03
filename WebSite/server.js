3a07f7ce01dcd808932eb5506468796af84e2968
Boc <radu.boc@haufe-lexware.com>
2017-10-03 12:03:03 +0300

    Hello world 4

--- WebSite/server.js
+++ WebSite/server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.use(function (req, res, next) {
-   res.send("Hello World 3");
+   res.send("Hello World 4");
 });
 
 app.set('port', 80);
